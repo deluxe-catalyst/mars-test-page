4bae8f3077ae0e0a38c8d53ee8d4768192163be6
Vladislav <vlafpav46@gmail.com>
2024-09-22 15:44:19 +0300

    Исправление хука

--- src/hooks/useMouseMove.ts
+++ src/hooks/useMouseMove.ts
@@ -1,28 +1,43 @@
 import { useEffect } from "react";
 
 export default function useMouseMove(callback: (x: number, y: number) => void) {
-  useEffect(() => {
-    const handleMouseMove = (event: MouseEvent) => {
-      if (window.innerWidth < 991) {
-        callback(0, 0);
-      } else {
-        const { clientX, clientY } = event;
-        const x = (clientX / window.innerWidth) - 0.5;
-        const y = (clientY / window.innerHeight) - 0.5;
-        callback(x, y);
-      }
-    };
 
-    const handleResize = () => {
-      callback(0, 0);
-    };
 
-    window.addEventListener('mousemove', handleMouseMove);
-    window.addEventListener('resize', handleResize);
 
-    return () => {
-      window.removeEventListener('mousemove', handleMouseMove);
-      window.removeEventListener('resize', handleResize);
-    };
-  }, [callback]);
+    useEffect(() => {
+        const handleOrientation = (event: DeviceOrientationEvent) => {
+            if(window.innerWidth <= 991) {
+                const { beta, gamma } = event;
+                if (beta !== null && gamma !== null) {
+                    const x = gamma / 90;  
+                    const y = beta / 180;
+                    callback(x, y);
+                }
+            }
+
+        };
+        const handleMouseMove = (event: MouseEvent) => {
+            if(window.innerWidth > 991) {
+                const { clientX, clientY } = event;
+                const x = (clientX / window.innerWidth) - 0.5;
+                const y = (clientY / window.innerHeight) - 0.5;
+                callback(x, y);
+            }
+        };
+
+
+        const handleResize = () => {
+            callback(0, 0);
+        };
+
+        window.addEventListener('deviceorientation', handleOrientation);
+        window.addEventListener('mousemove', handleMouseMove);
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+            window.removeEventListener('deviceorientation', handleOrientation);
+            window.removeEventListener('mousemove', handleMouseMove);
+        };
+    }, []);
 }
