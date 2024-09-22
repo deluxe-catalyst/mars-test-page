2cc24ac27d8e3f1836ac47ddaacaad4d8142ddea
Vladislav <vlafpav46@gmail.com>
2024-09-22 13:58:33 +0300

    фикс деплоя

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mars-test-page',
+  base: '/dist/',
   css: {
     preprocessorOptions: {
       scss: {
-        api: 'modern-compiler' // or "modern"
+        api: 'modern-compiler/'
       }
     }
   }
