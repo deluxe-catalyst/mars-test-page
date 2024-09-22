7de3aaec9c1853723b786d6f2d4577c485ea158c
Vladislav <vlafpav46@gmail.com>
2024-09-22 13:44:50 +0300

    очередной фикс деплоя

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mars-test-page/',
+  base: '/mars-test-page',
   css: {
     preprocessorOptions: {
       scss: {
