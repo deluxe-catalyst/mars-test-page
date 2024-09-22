6592933638666fa3ff52557b441d2f865f216835
Vladislav <vlafpav46@gmail.com>
2024-09-22 15:13:15 +0300

    логика хука обновлена, отключение параллакса на мобильной версии

--- src/hooks/useMouseMove.ts
+++ src/hooks/useMouseMove.ts
@@ -1,20 +1,28 @@
 import { useEffect } from "react";
 
 export default function useMouseMove(callback: (x: number, y: number) => void) {
-    useEffect(() => {
-      const handleMouseMove = (event: MouseEvent) => {
+  useEffect(() => {
+    const handleMouseMove = (event: MouseEvent) => {
+      if (window.innerWidth < 991) {
+        callback(0, 0);
+      } else {
         const { clientX, clientY } = event;
         const x = (clientX / window.innerWidth) - 0.5;
         const y = (clientY / window.innerHeight) - 0.5;
-  
         callback(x, y);
-      };
-  
-      window.addEventListener('mousemove', handleMouseMove);
-  
-      return () => {
-        window.removeEventListener('mousemove', handleMouseMove);
-      };
-    }, [callback]);
-  }
-  
+      }
+    };
+
+    const handleResize = () => {
+      callback(0, 0);
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [callback]);
+}
